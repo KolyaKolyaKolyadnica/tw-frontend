4fe81e7f64ca57acbf1db40a04a729898d06bede
AndreyAndreyQ11 <aayy6491@gmail.com>
2025-05-16 10:53:07 +0300

    add QR code block

---
+++ src/components/QRCodeComponent/QRCodeComponent.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import QRCodeStyling from "qr-code-styling";
+
+export default function QRCodeComponent({ text }: { text: string }) {
+  const ref = useRef<HTMLDivElement>(null);
+  const qrCode = React.useMemo(() => {
+    return new QRCodeStyling({
+      width: 100,
+      height: 100,
+      type: "svg",
+      data: "",
+      image: "",
+      dotsOptions: {
+        color: "#4267b2",
+        type: "rounded",
+      },
+      backgroundOptions: {
+        color: "#e9ebee",
+      },
+      imageOptions: {
+        crossOrigin: "anonymous",
+        margin: 20,
+      },
+    });
+  }, []);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.innerHTML = "";
+      qrCode.append(ref.current);
+    }
+  }, [qrCode]);
+
+  useEffect(() => {
+    qrCode.update({ data: text });
+  }, [text, qrCode]);
+
+  return <div ref={ref} className="w-10 h-10" />;
+}
